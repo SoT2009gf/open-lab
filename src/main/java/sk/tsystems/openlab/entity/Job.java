1310ae0bfa58cc0f0635c4af5d2bd4065e886002
Frantisek Kafka <58061897+SoT2009gf@users.noreply.github.com>
2020-01-24 17:29:36 +0100

    Removed unused entity column "general description".

--- src/main/java/sk/tsystems/openlab/entity/Job.java
+++ src/main/java/sk/tsystems/openlab/entity/Job.java
@@ -19,21 +19,15 @@ public class Job {
 	private String salary;
 	@Column(length = 8000)
 	private String requirements;
-	@Column(length = 8000)
-	private String generalDescription;
 	
-	public Job(String position, String employmentType, String applicationDeadline, String accountabilities, String salary, String requirements, String generalDescription) {
+	public Job(String position, String employmentType, String applicationDeadline, String accountabilities, String salary, String requirements) {
 		this.position = position;
 		this.employmentType = employmentType;
 		this.applicationDeadline = applicationDeadline;
 		this.accountabilities = accountabilities;
 		this.salary = salary;
 		this.requirements = requirements;
-		this.generalDescription = generalDescription;
-		
 	}
-
-	
 
 	public Job() {
 
@@ -88,13 +82,5 @@ public class Job {
 
 	public void setRequirements(String requirements) {
 		this.requirements = requirements;
-	}
-	
-	public String getGeneralDescription() {
-		return generalDescription;
-	}
-
-	public void setGeneralDescription(String generalDescription) {
-		this.generalDescription = generalDescription;
 	}
 }
