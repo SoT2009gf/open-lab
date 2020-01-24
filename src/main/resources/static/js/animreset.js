6d91fd05baac1332e38f45a629c3f983e5ae872b
Frantisek Kafka <58061897+SoT2009gf@users.noreply.github.com>
2020-01-24 17:56:14 +0100

    Pripravene na vikend.

--- src/main/resources/static/js/animreset.js
+++ src/main/resources/static/js/animreset.js
@@ -1,14 +1,24 @@
 var animation1 = document.getElementsByClassName("row1");
 var animation2 = document.getElementsByClassName("row2");
+var animation3 = document.getElementsByClassName("row3");
+var animation4 = document.getElementsByClassName("row4");
 var barAnimation = document.getElementsByClassName("in");
 const jobs1 = document.getElementsByClassName("loaded-jobs1");
 const jobs2 = document.getElementsByClassName("loaded-jobs2");
+const jobs3 = document.getElementsByClassName("loaded-jobs3");
+const jobs4 = document.getElementsByClassName("loaded-jobs4");
 const jobdesc1 = document.getElementsByClassName("jobdesc1");
 const jobdesc2 = document.getElementsByClassName("jobdesc2");
 const qrCodes1 = document.getElementsByClassName("qr-codes1");
 const qrCodes2 = document.getElementsByClassName("qr-codes2");
-const position1 = document.getElementsByClassName("positions1");
-const position2 = document.getElementsByClassName("positions2");
+const jobdesc3 = document.getElementsByClassName("jobdesc3");
+const jobdesc4 = document.getElementsByClassName("jobdesc4");
+const qrCodes3 = document.getElementsByClassName("qr-codes3");
+const qrCodes4 = document.getElementsByClassName("qr-codes4");
+const positions1 = document.getElementsByClassName("positions1");
+const positions2 = document.getElementsByClassName("positions2");
+const positions3 = document.getElementsByClassName("positions3");
+const positions4 = document.getElementsByClassName("positions4");
 
 var index = 0;
 var job1;
@@ -17,14 +27,23 @@ var qrCode1;
 var qrCode2;
 var animation1clone;
 var animation2clone;
-var barAnimClone;
+var job3;
+var job4;
+var qrCode3;
+var qrCode4;
+var animation3clone;
+var animation4clone;
 var jobdesctemp1;
 var jobdesctemp2;
+var jobdesctemp3;
+var jobdesctemp4;
 var postemp1;
 var postemp2;
+var postemp3;
+var postemp4;
 
 slide();
-setInterval(slide, 10000);
+setInterval(slide, 30000);
 
 function slide() {
 	if (job1) {
@@ -50,14 +69,42 @@ function slide() {
 		postemp2.classList.add("hidden");
 	}
 
+	if (job3) {
+		job3.classList.remove("displayed");
+		job3.classList.add("hidden");
+		jobdesctemp3.classList.remove("displayed");
+		jobdesctemp3.classList.add("hidden");
+		qrCode3.classList.remove("displayed");
+		qrCode3.classList.add("hidden");
+		postemp3.classList.remove("displayed");
+		postemp3.classList.add("hidden");
+	}
+	
+	if (job4) {
+		job4.classList.remove("displayed");
+		job4.classList.add("hidden");
+		jobdesctemp4.classList.remove("displayed");
+		jobdesctemp4.classList.add("hidden");
+		qrCode4.classList.remove("displayed");
+		qrCode4.classList.add("hidden");
+		postemp4.classList.remove("displayed");
+		postemp4.classList.add("hidden");
+	}
+
 	animation1clone = animation1[0].cloneNode(true);
 	animation1[0].parentNode.replaceChild(animation1clone, animation1[0]);
 	animation2clone = animation2[0].cloneNode(true);
 	animation2[0].parentNode.replaceChild(animation2clone, animation2[0]);
+	animation3clone = animation3[0].cloneNode(true);
+	animation3[0].parentNode.replaceChild(animation3clone, animation3[0]);
+	animation4clone = animation4[0].cloneNode(true);
+	animation4[0].parentNode.replaceChild(animation4clone, animation4[0]);
 	barAnimClone = barAnimation[0].cloneNode(true);
 	barAnimation[0].parentNode.replaceChild(barAnimClone, barAnimation[0]);
 	animation1[0] = animation1clone;
 	animation2[0] = animation2clone;
+	animation3[0] = animation3clone;
+	animation4[0] = animation4clone;
 	barAnimation[0] = barAnimClone;	
 
 	if (index > (jobs1.length - 1)) {
@@ -70,13 +117,13 @@ function slide() {
 	jobdesc1[index].classList.add("displayed");
 	qrCodes1[index].classList.remove("hidden");
 	qrCodes1[index].classList.add("displayed");
-	position1[index].classList.remove("hidden");
-	position1[index].classList.add("displayed");
+	positions1[index].classList.remove("hidden");
+	positions1[index].classList.add("displayed");
 
 	job1 = jobs1[index];
 	qrCode1 = qrCodes1[index];
 	jobdesctemp1 = jobdesc1[index];
-	postemp1 = position1[index];
+	postemp1 = positions1[index];
 
 	index += 1;
 
@@ -90,13 +137,53 @@ function slide() {
 	jobdesc2[index].classList.add("displayed");
 	qrCodes2[index].classList.remove("hidden");
 	qrCodes2[index].classList.add("displayed");
-	position2[index].classList.remove("hidden");
-	position2[index].classList.add("displayed");
+	positions2[index].classList.remove("hidden");
+	positions2[index].classList.add("displayed");
 
 	job2 = jobs2[index];
 	qrCode2 = qrCodes2[index];
 	jobdesctemp2 = jobdesc2[index];
-	postemp2 = position2[index];
+	postemp2 = positions2[index];
+
+	index += 1;
+
+	if (index > (jobs1.length - 1)) {
+		index = 0;
+	}
+
+	jobs3[index].classList.remove("hidden");
+	jobs3[index].classList.add("displayed");
+	jobdesc3[index].classList.remove("hidden");
+	jobdesc3[index].classList.add("displayed");
+	qrCodes3[index].classList.remove("hidden");
+	qrCodes3[index].classList.add("displayed");
+	positions3[index].classList.remove("hidden");
+	positions3[index].classList.add("displayed");
+
+	job3 = jobs3[index];
+	qrCode3 = qrCodes3[index];
+	jobdesctemp3 = jobdesc3[index];
+	postemp3 = positions3[index];
+
+	index += 1;
+
+	if (index > (jobs1.length - 1)) {
+		index = 0;
+	}
+
+	jobs4[index].classList.remove("hidden");
+	jobs4[index].classList.add("displayed");
+	jobdesc4[index].classList.remove("hidden");
+	jobdesc4[index].classList.add("displayed");
+	qrCodes4[index].classList.remove("hidden");
+	qrCodes4[index].classList.add("displayed");
+	positions4[index].classList.remove("hidden");
+	positions4[index].classList.add("displayed");
+
+	job4 = jobs4[index];
+	qrCode4 = qrCodes4[index];
+	jobdesctemp4 = jobdesc4[index];
+	postemp4 = positions4[index];
 
 	index += 1;
 }
