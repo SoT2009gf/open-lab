3bb49b5a44cd96ba18d92f2dec98dd7c670f0e77
Frantisek Kafka <58061897+SoT2009gf@users.noreply.github.com>
2020-01-27 17:01:23 +0100

    Work progress.

--- src/main/resources/static/js/select.js
+++ src/main/resources/static/js/select.js
@@ -1,11 +1,11 @@
 const vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 const vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
-/*if(vw <= 1080 && vh <= 1920) {
-	location.href="/screen1x1";
+/*if(vw <= 3840) {
+	location.href="/team1/screen1x1";
 }
 
-if(vw <= 3840  && vh <= 2160) {
-	location.href="/screen2x2";
+if(vw > 3840) {
+	location.href="/team1/screen2x2";
 }*/
-location.href="/screen1x1";
+location.href="/screen2x2";
