4d56f25ffb654624de9a58bd29ca46d35b6ccffb
Frantisek Kafka <58061897+SoT2009gf@users.noreply.github.com>
2020-01-29 14:10:48 +0100

    finished screen selector

--- src/main/resources/static/js/select.js
+++ src/main/resources/static/js/select.js
@@ -1,11 +1,8 @@
 const vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 const vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
-/*if(vw <= 3840) {
+if(vw <= 1080) {
 	location.href="/team1/screen1x1";
-}
-
-if(vw > 3840) {
+} else {
 	location.href="/team1/screen2x2";
-}*/
-location.href="/screen2x2";
+}
